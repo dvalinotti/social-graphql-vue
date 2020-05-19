f743bc7fd35eecc26661e2bf545a1621695dce09
Dan Valinotti <dan_valinotti@colpal.com>
2020-05-19 14:15:29 -0400

    Add desktop icon component to open main window

---
+++ src/components/DesktopIcon.vue
@@ -0,0 +1,81 @@
+<template>
+  <div
+    v-click-outside="onClickOutside"
+    class="desktop-icon"
+    @click="onClickInside"
+  >
+    <div
+      :class="{ clicked: clicked }"
+      class="icon-container"
+    >
+      <img
+        src="../assets/logo.png"
+        alt="Desktop icon"
+      >
+    </div>
+    <div
+      class="text-container"
+      :class="{ clicked: clicked }"
+    >
+      <p>Social GraphQL</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import ClickOutside from 'vue-click-outside';
+
+export default {
+  directives: {
+    ClickOutside,
+  },
+  props: {
+    click: {
+      type: Function,
+      default: () => console.log('click'),
+    },
+  },
+  data: () => ({
+    clicked: false,
+  }),
+  methods: {
+    onClickInside() {
+      this.click();
+      this.$data.clicked = true;
+    },
+    onClickOutside() {
+      this.$data.clicked = false;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+  .desktop-icon {
+    width: 60px;
+    height: 70px;
+    position: absolute;
+    left: 50px;
+    top: 50px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .text-container {
+      width: 100%;
+      font-size: 0.8em;
+      text-align: center;
+      font-family: "Pixelated MS Sans Serif",Arial;
+      padding: 4px;
+      color: #fafafa;
+    }
+    .icon-container {
+      width: 45px;
+      padding: 4px;
+    }
+  }
+  .clicked {
+    background-color: #0053f0;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <div class="desktop">
+      <DesktopIcon :click="onClickIcon" />
       <transition
         appear
         name="scale-up"
@@ -8,6 +9,8 @@
         <div v-if="windowOpen">
           <vue-draggable-resizable
             :parent="true"
+            :w="auto"
+            :h="auto"
             drag-handle=".title-bar"
             @dragging="onDrag"
           >
@@ -22,7 +25,10 @@
                   <div class="title-bar-controls">
                     <button aria-label="Minimize" />
                     <button aria-label="Maximize" />
-                    <button aria-label="Close" />
+                    <button
+                      aria-label="Close"
+                      @click="closeWindow"
+                    />
                   </div>
                 </div>
                 <Login />
@@ -38,19 +44,22 @@
 <script>
 import VueDraggableResizable from 'vue-draggable-resizable';
 import Login from '../windows/Login';
+import DesktopIcon from '../components/DesktopIcon';
 
 export default {
   name: 'Home',
   components: {
     Login,
     VueDraggableResizable,
+    DesktopIcon,
   },
   data: () => ({
-    windowOpen: true,
+    windowOpen: false,
     width: 0,
     height: 0,
     x: 0,
     y: 0,
+    clicks: 0,
   }),
   methods: {
     onResize(x, y, width, height) {
@@ -63,6 +72,16 @@ export default {
       this.x = x;
       this.y = y;
     },
+    onClickIcon() {
+      this.$data.clicks += 1;
+      if (this.$data.clicks === 2) {
+        this.$data.windowOpen = true;
+        this.$data.clicks = 0;
+      }
+    },
+    closeWindow() {
+      this.$data.windowOpen = false;
+    },
   },
 };
 </script>
@@ -74,11 +93,13 @@ export default {
     margin: 0 auto;
     position: relative;
   }
+  .desktop {
+    position: relative;
+  }
 }
 .scale-up-enter-active {
   transition: transform 0.1s cubic-bezier(.5,0,.7,.8),
     opacity 0.01s linear;
-  transition-delay: 750ms;
 }
 .scale-up-leave-active {
   transition: transform 0.1s cubic-bezier(.5,0,.7,.8),
